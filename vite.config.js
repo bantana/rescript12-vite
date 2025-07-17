b8d26e552e961ea1c58de88d81ce7eaa1cd82b49
bantana <bantana@aozsky.com>
2025-07-17 16:56:17 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import rescript from "@jihchi/vite-plugin-rescript";
+// import rescript from "@jihchi/vite-plugin-rescript";
+import rescript from "@nojaf/vite-plugin-rescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
